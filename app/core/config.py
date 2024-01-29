596aee99572e689b517b29968b9046de9d7781f4
Fill <tima.filicheff1@yandex.ru>
2024-01-29 09:23:07 +0300

    поправил названия переменных из env

--- app/core/config.py
+++ app/core/config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-user = os.getenv("USER")
-password = os.getenv("PASSWORD")
+user = os.getenv("POSTGRES_USER")
+password = os.getenv("POSTGRES_PASSWORD")
 host = os.getenv("HOST")
 port = os.getenv("PORT")
 db_name = os.getenv("DB_NAME")
 
-test_user = os.getenv("TEST_USER")
-test_password = os.getenv("TEST_PASSWORD")
+test_user = os.getenv("POSTGRES_USER")
+test_password = os.getenv("POSTGRES_PASSWORD")
 test_host = os.getenv("TEST_HOST")
 test_port = os.getenv("TEST_PORT")
 test_db_name = os.getenv("TEST_DB_NAME")
